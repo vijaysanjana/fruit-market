df68954f28c674e41be5c4b0c25494dca7c8e593
theNareynater <whitacre@purdue.edu>
2022-11-11 17:45:20 -0500

    fixed Main, removed store field from Product class

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,26 @@
+import java.util.*;
+
+
 class Main {
     public static void main(String[] args) {
         //System.out.println("Hello world!");
 
         Store store = new Store("Denny's", "it's... Denny's");
-        store.addProduct(new Product("Gamer Eggs", "Nature's Protein Shake", store, 19.99));
+        store.addProduct(new Product("Gamer Eggs", "Nature's Protein Shake", 19.99));
 
 
         System.out.print("\n\n\n");
         Seller user = new Seller("nathanWhitacre", "whitacre@purdue.edu", "gamerGod88");
 
-        Store store = new Store("God's Soap Emporium", "Got bored, like soap - God", user);
+        Store store2 = new Store("God's Soap Emporium", "Got bored, like soap - God");
         ArrayList<Store> stores = new ArrayList<>();
-        stores.add(store);
+        stores.add(store2);
         user.setStores(stores);
 
-        Product product = new Product("Dawn PowerWash Dish Spray", "clean up you gross ass plate", store, 19.99);
+        Product product = new Product("Dawn PowerWash Dish Spray", "clean up you gross ass plate", 19.99);
         ArrayList<Product> products = new ArrayList<>();
         products.add(product);
-        store.setProducts(products);
+        store2.setProducts(products);
 
         System.out.printf("%s presents,\na brand new %s product:\n%s\n", user.getUsername(), user.getStores().get(0).getName(), user.getStores().get(0).getProducts().get(0).getName());
     }
