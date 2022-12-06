fc27c11d2bdd3c8c6a9e10efa7432bdf472d0c71
theNareynater <whitacre@purdue.edu>
2022-12-06 12:04:40 -0500

    Login test with server-client

--- src/ClientCore.java
+++ src/ClientCore.java
@@ -5,23 +5,54 @@ import java.net.*;
 // Client class
 class ClientCore {
 
+    public static final String separator = "---------------------------";
+
     // driver code
     public static void main(String[] args)
     {
         // establish a connection by providing host and port
         // number
         try (Socket socket = new Socket("localhost", 9999)) {
+            Scanner sc = new Scanner(System.in); //Test Scanner TODO: Remove Terminal Input
 
             //Server Input and Output
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-            String request = null; //Request to be sent to ServerCore
+            String request = ""; //Request to be sent to ServerCore
+            String response = null; //Response to be sent from ServerCore
             String location = "1.0"; //Address of GUI currently visible to user
             while (!request.equalsIgnoreCase("Quit")) {
 
                 switch (location) {
-                    case "1.0":
+                    case "1.0": //Login Page
+                        String email;
+                        String password;
+                        String loginSignup;
+                        String customerSeller;
+                        String username;
+
+                        System.out.println("Welcome to The Marketplace!");
+                        System.out.println(separator);
+                        System.out.println("Login or signup to use our service.");
+                        System.out.println("Please enter: " + "\n[1] Login" + "\n[2] Signup");
+                        loginSignup = sc.nextLine();
+                        if (loginSignup.equalsIgnoreCase("1")) {
+                            System.out.println(separator);
+                            System.out.println("Please enter your email: ");
+                            email = sc.nextLine();
+                            System.out.println("Please enter your password: ");
+                            password = sc.nextLine();
+
+                            request = "{Login}" + email + "," + password + ",";
+                            out.println(request);
+
+                            response = in.readLine();
+                            customerSeller = response.substring((response.indexOf("}") + 1), response.indexOf(","));
+                            username = response.substring((response.indexOf(",") + 1), response.lastIndexOf(","));
+                            System.out.println(separator);
+                            System.out.println("Welcome customer: " + username);
+                        }
                         //Open Login GUI
                         //Read User Input
                         //Close Login GUI
